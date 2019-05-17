42a8cee234f957bcf7aa1906e1374a265ed5568b
杨海涛 <yannis_123@live.com>
2019-05-17 15:30:47 +0800

    add xadmin

--- src/ymapp-aspnet-core/src/YMApp.Web.Admin/Controllers/ProductController.cs
+++ src/ymapp-aspnet-core/src/YMApp.Web.Admin/Controllers/ProductController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using YMApp.Controllers;
 
 namespace YMApp.Web.Admin.Controllers
 {
-    public class ProductController : Controller
+    public class ProductController : YMAppControllerBase
     {
         public IActionResult Index()
         {
