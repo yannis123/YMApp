102209ff7eec0e608b76501c4a232ac97340ea9f
杨海涛 <yannis_123@live.com>
2019-09-24 17:12:15 +0800

    add react project

--- src/ymapp-angular/src/shared/auth/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './app-auth.service';
-export * from './auth-route-guard';
-export * from './permission.service';
--- src/ymapp-angular/src/shared/components/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './validation-messages/validation-messages.component';
-export * from './no-data/no-data.component';
-
-
-export { CustomComponentModule } from './custom-components.module';
-
-
